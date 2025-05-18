5c5bc39b85f0b9a174bd3eee0349c62b32a195a8
sxv3 <66145858+sxv3@users.noreply.github.com>
2025-05-17 20:03:37 -0700

    added rotate function

--- RBTree.cpp
+++ RBTree.cpp
@@ -15,3 +15,44 @@ RBTree::RBTree() {
   //node initialize
   root = sentinel;
 }
+
+void RBTree::rotate(Node* x, bool isRight) {
+  if (isRight == false) {
+    Node* y = x->right;
+    x->right = y->left;
+    if (y->left != sentinel) {
+      y->left->parent = x;
+    }
+    
+    y->parent = x->parent;
+    if (x->parent == sentinel) {
+      root = y;
+    } else if (x == x->parent->left) {
+      x->parent->left = y;
+    } else {
+      x->parent->right = y;
+    }
+    
+    y->left = x;
+    x->parent = y;  
+  } else {
+    Node* y = x->left;
+    x->left = y->right;
+    if (y->right != sentinel) {
+        y->right->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == sentinel) {
+        root = y;
+    } else if (x == x->parent->right) {
+        x->parent->right = y;
+    } else {
+        x->parent->left = y;
+    }
+    y->right = x;
+    x->parent = y;
+  }
+}
+
+
+  
