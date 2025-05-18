1aa09d7e00ef3adfa21a3ca60d0cc8dd38ef8b3a
sxv3 <66145858+sxv3@users.noreply.github.com>
2025-05-17 17:01:56 -0700

    Create RBTree.h

---
+++ RBTree.h
@@ -0,0 +1,30 @@
+#ifndef RBTREE_H
+#define RBTREE_H
+#include "Node.h"
+//rbtree header 
+//by Tai Wong, 5/17/25
+
+class RBTree {
+private:
+
+  Node* root;
+  
+  void rotateLeft(Node* node);
+
+  void rotateRight(Node* node);
+
+  void rebalanceTree(Node* node);
+
+  void printHelper(Node* root, int count, bool end);
+
+public:
+
+  RBTree();
+  ~RBTree();
+
+  void insert(int val);
+  void load(const char* filename);
+  void print();
+};
+
+#endif
